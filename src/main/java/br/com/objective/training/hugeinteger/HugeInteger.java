6bfb21175c10820fec2856c1fb4887ed74ec89d7
igorarouca <igor.arouca@gmail.com>
2011-06-06 11:46:40 -0300

    Calculator implemented using Generics

--- src/main/java/br/com/objective/training/hugeinteger/HugeInteger.java
+++ src/main/java/br/com/objective/training/hugeinteger/HugeInteger.java
@@ -1,17 +1,6 @@
 package br.com.objective.training.hugeinteger;
 
-public interface HugeInteger {
-
-	/**
-	 *	other < this --> -1
-	 *	other = this -->  0
-	 *	other > this --> +1
-	 */
-	public abstract int compareTo(HugeInteger value);
-
-	public abstract HugeInteger add(HugeInteger value);
-
-	public abstract HugeInteger subtract(HugeInteger value);
+public interface HugeInteger extends UnaryOperations<HugeInteger> {
 
 	public abstract HugeInteger negate();
 
--- src/main/java/br/com/objective/training/hugeinteger/HugeIntegerFactory.java
+++ src/main/java/br/com/objective/training/hugeinteger/HugeIntegerFactory.java
@@ -1,23 +1,11 @@
 package br.com.objective.training.hugeinteger;
 
-import br.com.objective.training.foundation.Functor;
-
 public class HugeIntegerFactory {
 
-	private static HugeIntegerFactory instance;
-
-	private final Functor<String, HugeInteger> stringToHugeInteger;
-
-	public HugeIntegerFactory(Functor<String, HugeInteger> functor) {
-		this.stringToHugeInteger = functor;
-	}
-
-	public static void load(HugeIntegerFactory factory) {
-		instance = factory;
-	}
+	private HugeIntegerFactory() {};
 
 	public static HugeInteger newHugeInteger(String value) {
-		return instance.stringToHugeInteger.evaluate(value);
+		return new HugeIntegerImpl(value);
 	}
 
 }
