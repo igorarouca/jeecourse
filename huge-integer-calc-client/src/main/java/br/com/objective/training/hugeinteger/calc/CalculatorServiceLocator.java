eccb2986fb508f9112761efc0bb2107a07043583
Igor Arouca <igor.arouca@gmail.com>
2011-06-19 20:22:27 -0300

    CalculatorStub moved out of CalculatorServiceLocator

--- huge-integer-calc-client/src/main/java/br/com/objective/training/hugeinteger/calc/CalculatorServiceLocator.java
+++ huge-integer-calc-client/src/main/java/br/com/objective/training/hugeinteger/calc/CalculatorServiceLocator.java
@@ -1,17 +1,9 @@
 package br.com.objective.training.hugeinteger.calc;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 
 import br.com.objective.training.foundation.Producer;
-import br.com.objective.training.hugeinteger.calc.protocol.Add;
-import br.com.objective.training.hugeinteger.calc.protocol.Compare;
-import br.com.objective.training.hugeinteger.calc.protocol.Operation;
-import br.com.objective.training.hugeinteger.calc.protocol.Subtract;
 
 public class CalculatorServiceLocator {
 
@@ -41,55 +33,6 @@ public class CalculatorServiceLocator {
 
 	private static int defaultPort() {
 		return 9876;
-	}
-
-	private static class CalculatorStub implements Calculator<String> {
-
-		private final Socket socket;
-		private final ObjectInputStream inputStream;
-		private final ObjectOutputStream outputStream;
-
-		private CalculatorStub(InetAddress ip, int port) {
-			try {
-				this.socket = new Socket(ip, port);
-				this.inputStream = new ObjectInputStream(socket.getInputStream());
-				this.outputStream = new ObjectOutputStream(socket.getOutputStream());
-
-			} catch (IOException ioe) {
-				throw new RuntimeException("Error trying to initialize calculator");
-			}
-		}
-		
-		@Override
-		public String compare(String left, String right) {
-			return sendRequest(new Compare(left, right));
-		}
-
-		@Override
-		public String add(String left, String right) {
-			return sendRequest(new Add(left, right));
-		}
-
-		@Override
-		public String subtract(String left, String right) {
-			return sendRequest(new Subtract(left, right));
-		}
-
-		private String sendRequest(Operation operation) {
-			String result = null;
-			System.out.println("Sending request: " + operation);
-			try {
-				outputStream.writeObject(operation);
-				result = inputStream.readObject().toString();
-
-			} catch (Exception e) {
-				e.printStackTrace(System.err);
-				throw new RuntimeException("Error computing operation");
-			}
-
-			return result;
-		}
-
 	}
 
 }
