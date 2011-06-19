957b481b77e0cc83a5ff2d8e14013353c95451ac
Igor Arouca <igor.arouca@gmail.com>
2011-06-19 20:24:13 -0300

    Minor change in log messages

--- huge-integer-calc-server/src/main/java/br/com/objective/training/hugeinteger/calc/CalculatorServer.java
+++ huge-integer-calc-server/src/main/java/br/com/objective/training/hugeinteger/calc/CalculatorServer.java
@@ -8,9 +8,9 @@ public class CalculatorServer {
 	public static void main(String[] args) throws IOException {
 		Calculator<String> calculator = CalculatorFactory.load(ProducerUsingHugeInteger.getInstance()).newCalculator();
 		Service service = CalculatorServiceWrapper.wrap(calculator);
-		System.out.println("Starting calculator service...");
+		System.out.println(">>> Starting calculator service...");
 		service.startOn(InetAddress.getLocalHost(), 9876);
-		System.out.println("Calculator service running.");		
+		System.out.println(">>> Calculator service running.");		
 	}
 
 }
